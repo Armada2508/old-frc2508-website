c511451e3159a246b5727f089cedd78016c7cef9
Kylo <me@kylo.io>
2015-03-01 11:51:57 -0600

    Base layout.

---
+++ config.rb
@@ -0,0 +1,13 @@
+# Assets
+set :css_dir, 'assets/stylesheets'
+set :js_dir, 'assets/javascripts'
+set :images_dir, 'assets/images'
+
+# Pretty URLs
+activate :directory_indexes
+
+# Production
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+end
---
+++ source/assets/javascripts/custom.js
@@ -0,0 +1,10 @@
+
+$('body').scrollspy({
+  'target': '#nav',
+  'offset': 40
+});
+
+
+$('.carousel').carousel({
+  interval: 5000
+});
