c3fd0917c26a9ea3b9c49f2e25b4e1099ee41a32
Monica Anuforo <monica.anuforo@gmail.com>
2015-04-03 01:16:29 -0500

    Site to make Live
    Temp site. Not a flawless dream, but passable for competition.

--- source/assets/javascripts/custom.js
+++ source/assets/javascripts/custom.js
@@ -4,7 +4,19 @@ $('body').scrollspy({
   'offset': 40
 });
 
+$('.carousel').each(function() {
+  var speed = 5000;
+  if ($(this).attr('rel-speed') != null) {
+  	speed = $(this).attr('rel-speed');
+  }
+  $(this).carousel({
+  	interval: speed
+  });
+});
 
-$('.carousel').carousel({
-  interval: 5000
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
 });
